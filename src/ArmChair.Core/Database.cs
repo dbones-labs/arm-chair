50b356bb0924fdcf1a9debec34844d9380008996
dbones <dbones@users.noreply.github.com>
2014-09-07 23:40:05 +0100

    updated header

--- src/ArmChair.Core/Database.cs
+++ src/ArmChair.Core/Database.cs
@@ -1,16 +1,16 @@
-﻿// // Copyright 2013 - 2014 dbones.co.uk (David Rundle)
-// // 
-// // Licensed under the Apache License, Version 2.0 (the "License");
-// // you may not use this file except in compliance with the License.
-// // You may obtain a copy of the License at
-// // 
-// //     http://www.apache.org/licenses/LICENSE-2.0
-// // 
-// // Unless required by applicable law or agreed to in writing, software
-// // distributed under the License is distributed on an "AS IS" BASIS,
-// // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// // See the License for the specific language governing permissions and
-// // limitations under the License.
+﻿// Copyright 2013 - 2014 dbones.co.uk (David Rundle)
+//  
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 namespace ArmChair
 {
     using System.Collections.Generic;
@@ -18,27 +18,34 @@ namespace ArmChair
     using Http;
     using Serialization;
 
+    /// <summary>
+    /// Represents the CouchDB, allowing execution of supported commands.
+    /// </summary>
     public class Database
     {
         private readonly string _name;
         private readonly IConnection _connection;
         private readonly ISerializer _serializer;
 
+
+        /// <summary>
+        /// Create an instance of a database
+        /// </summary>
+        /// <param name="name">name of the database</param>
+        /// <param name="connection">the connection to the database</param>
+        /// <param name="serializer">the serializer to use when exxchanging data</param>
         public Database(string name, Connection connection, ISerializer serializer)
         {
             _name = name;
             _connection = connection;
             _serializer = serializer;
-
-
-            //_settings = new JsonSerializerSettings();
-            //_settings.TypeNameAssemblyFormat = FormatterAssemblyStyle.Simple;
-            //_settings.TypeNameHandling = TypeNameHandling.Auto;
-            //_settings.ContractResolver = new ContractResolver();
-            //_serializer = JsonSerializer.Create(_settings);
         }
 
-
+        /// <summary>
+        /// Load a single Document
+        /// </summary>
+        /// <param name="key">the key/id of the document</param>
+        /// <returns>returns the document in its POCO form</returns>
         public object LoadEntity(string key)
         {
             object entity = null;
@@ -54,6 +61,11 @@ namespace ArmChair
             return entity;
         }
 
+        /// <summary>
+        /// Load a number of documents via their keys
+        /// </summary>
+        /// <param name="keys">keys/ids of the documents to load</param>
+        /// <returns>the documents loaded into their assocaited POCO's</returns>
         public AllDocsResponse LoadAllEntities(AllDocsRequest keys)
         {
             var requestJson = _serializer.Serialize(keys);
@@ -72,6 +84,11 @@ namespace ArmChair
             return result;
         }
 
+        /// <summary>
+        /// apply a number of changes in 1 bulk 
+        /// </summary>
+        /// <param name="updates"></param>
+        /// <returns></returns>
         public IEnumerable<BulkDocResponse> BulkApplyChanges(BulkDocsRequest updates)
         {
 
@@ -87,7 +104,7 @@ namespace ArmChair
                 var content = response.Content.ReadToEnd();
                 results = _serializer.Deserialize<IEnumerable<BulkDocResponse>>(content);
             });
-            //TODO: partial update the object before exit of method via serializer
+         
             return results;
         }
     }
