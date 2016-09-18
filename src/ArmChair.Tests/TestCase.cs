4a3a522fbea0bd7b5c03033b30ab8e854ecfc198
dbones <dbones@users.noreply.github.com>
2016-09-18 17:24:10 +0100

    running tests using a preview version of couchdb 2.0, seems the offset is optional now

--- src/ArmChair.Tests/TestCase.cs
+++ src/ArmChair.Tests/TestCase.cs
@@ -72,7 +72,7 @@ namespace ArmChair.Tests
         {
             OnFixtureTearDown();
 
-            EnsureDbIsDeleted();
+            //EnsureDbIsDeleted();
         }
 
         private void EnsureDbIsDeleted()
