5775eb0955b691c62db8fc9472620a092bc24efb
David Rundle <dave@dbones.co.uk>
2017-08-14 02:30:42 +0100

    correct resources

--- samples/todo/Todo.Service/Dto/Resources/Resource.cs
+++ samples/todo/Todo.Service/Dto/Resources/Resource.cs
@@ -3,35 +3,49 @@
     using System.Collections.Generic;
 
     /// <summary>
-    /// a REST resource, base class
+    /// the base of a resource class, do not inherit of this directly
+    /// </summary>
+    public abstract class ResourceBase
+    {
+        public Dictionary<string, string> Links { get; set; }
+        public Dictionary<string, string> Actions { get; set; }
+    }
+
+    /// <summary>
+    /// a REST resource, inherit of this for each resource
     /// </summary>
     /// <remarks>
     /// based off the Rancher spec
     /// https://github.com/rancher/api-spec/blob/master/specification.md#resources
     /// </remarks>
-    public abstract class Resource
+    public abstract class Resource : ResourceBase
     {
         public string Id { get; set; }
         public string Revision { get; set; }
-        public Dictionary<string, string> Links { get; set; }
-        public Dictionary<string, string> Actions { get; set; }
+
+        /*   
+        {
+            "id":      "b1b2e7006be", 
+            "type":    "file",
+            "links":   {  see links  },
+            "actions": { see actions  },
+            "name":    "ultimate_answer.txt",
+            "size":    2,
+            .... more stuff
+        }
+        */
+
     }
 
-    
-    public class CollectionResource<T> : Resource where T : Resource
+
+    /// <summary>
+    /// a collection of resources.
+    /// </summary>
+    /// <typeparam name="T">the type of the resource</typeparam>
+    public class CollectionResource<T> : ResourceBase where T : Resource
     {
         public List<T> Data { get; set; }
     }
 
-    /*   
-    {
-        "id":      "b1b2e7006be", 
-        "type":    "file",
-        "links":   {  see links  },
-        "actions": { see actions  },
-        "name":    "ultimate_answer.txt",
-        "size":    2,
-        .... more stuff
-    }
-*/
+    
 }
