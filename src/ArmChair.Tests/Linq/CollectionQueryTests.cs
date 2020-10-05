38051bad92ee14ca9871dc0f634cff67e8e5e240
david rundle <dbones@users.noreply.github.com>
2020-10-05 03:15:48 +0100

    updated with a extra query test

--- src/ArmChair.Tests/Linq/CollectionQueryTests.cs
+++ src/ArmChair.Tests/Linq/CollectionQueryTests.cs
@@ -3,6 +3,7 @@ namespace ArmChair.Tests.Linq
     using System.Collections.Generic;
     using System.Linq;
     using Domain.Sample1;
+    using Domain.Sample5;
     using NUnit.Framework;
 
     public class CollectionQueryTests : QueryTestCase
@@ -52,6 +53,22 @@ namespace ArmChair.Tests.Linq
             Assert.IsTrue(results.All(x => x.Name != "dave"));
         }
 
+        
+        [Test]
+        public void Sequence_property_contains_sequence()
+        {
+            List<ServiceEntry> results;
+            List<ServiceEntry> expected;
+            using (var session = Database.CreateSession())
+            {
+                var tags = new[] { "Relational", "Document" };
+                results = session.Query<ServiceEntry>().Where(x => tags.All()).ToList();
+                expected = Query<ServiceEntry>().Where(x => tags.All(y => x.Tags.Contains(y))).ToList();
+            }
+
+            Assert.AreEqual(expected.Count, results.Count);
+            Assert.IsTrue(expected.All(x => results.Any(y => y.Id == x.Id)));
+        }
 
     }
 }
